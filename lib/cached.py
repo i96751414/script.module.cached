ea1f9b27513db6262d56eebc96e73756196731b2
i96751414 <i96751414@gmail.com>
2020-11-26 00:18:57 +0000

    Initial version

---
+++ lib/cached.py
@@ -0,0 +1,154 @@
+import datetime
+import os
+import pickle
+import sqlite3
+from base64 import b64encode, b64decode
+from functools import wraps
+from hashlib import sha256
+
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+ADDON_DATA = xbmc.translatePath(xbmcaddon.Addon("script.module.cached").getAddonInfo("profile"))
+ADDON_NAME = xbmcaddon.Addon().getAddonInfo("name")
+
+if not os.path.exists(ADDON_DATA):
+    os.makedirs(ADDON_DATA)
+
+
+def pickle_hash(obj):
+    data = pickle.dumps(obj)
+    h = sha256()
+    h.update(data)
+    return h.hexdigest()
+
+
+class _BaseCache(object):
+    __instance = None
+
+    _load_func = staticmethod(pickle.loads)
+    _dump_func = staticmethod(pickle.dumps)
+    _hash_func = staticmethod(pickle_hash)
+
+    @classmethod
+    def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+
+    def get(self, key, default=None, hashed_key=False):
+        if not hashed_key:
+            key = self._hash_func(key)
+        result = self._get(key)
+        ret = default
+        if result:
+            data, expires = result
+            if expires > datetime.datetime.utcnow():
+                ret = self._process(data)
+        return ret
+
+    def set(self, key, data, expiry_time, hashed_key=False):
+        if not hashed_key:
+            key = self._hash_func(key)
+        self._set(key, self._prepare(data), datetime.datetime.utcnow() + expiry_time)
+
+    def _process(self, obj):
+        return obj
+
+    def _prepare(self, s):
+        return s
+
+    def _get(self, key):
+        raise NotImplementedError("_get needs to be implemented")
+
+    def _set(self, key, data, expires):
+        raise NotImplementedError("_set needs to be implemented")
+
+
+class MemoryCache(_BaseCache):
+    def __init__(self, database=ADDON_NAME):
+        self._window = xbmcgui.Window(10000)
+        self._database = database + "."
+
+    def _get(self, key):
+        data = self._window.getProperty(self._database + key)
+        return self._load_func(b64decode(data)) if data else None
+
+    def _set(self, key, data, expires):
+        self._window.setProperty(self._database + key, b64encode(self._dump_func((data, expires))).decode())
+
+
+class Cache(_BaseCache):
+    _table_name = "cached"
+
+    def __init__(self, database=os.path.join(ADDON_DATA, ADDON_NAME + ".cached.sqlite"),
+                 cleanup_interval=datetime.timedelta(minutes=15)):
+        self._conn = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
+        self._cursor = self._conn.cursor()
+        self._cursor.execute(
+            "CREATE TABLE IF NOT EXISTS `{}` ("
+            "key TEXT UNIQUE NOT NULL, "
+            "data BLOB NOT NULL, "
+            "expires TIMESTAMP NOT NULL"
+            ")".format(self._table_name))
+        self._conn.commit()
+        self._cleanup_interval = cleanup_interval
+        self._last_cleanup = datetime.datetime.utcnow()
+        self.clean_up()
+
+    def _get(self, key):
+        self.check_clean_up()
+        self._cursor.execute("SELECT data, expires FROM `{}` WHERE key = ?".format(self._table_name), (key,))
+        return self._cursor.fetchone()
+
+    def _process(self, obj):
+        return self._load_func(obj)
+
+    def _prepare(self, s):
+        return self._dump_func(s)
+
+    def _set(self, key, data, expires):
+        self.check_clean_up()
+        self._cursor.execute(
+            "INSERT OR REPLACE INTO `{}` (key, data, expires) VALUES(?, ?, ?)".format(self._table_name),
+            (key, data, expires))
+        self._conn.commit()
+
+    def clean_up(self):
+        self._cursor.execute(
+            "DELETE FROM `{}` WHERE expires <= STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')".format(self._table_name))
+        self._conn.commit()
+        self._last_cleanup = datetime.datetime.utcnow()
+
+    def check_clean_up(self):
+        clean_up = self._last_cleanup + self._cleanup_interval < datetime.datetime.utcnow()
+        if clean_up:
+            self.clean_up()
+        return clean_up
+
+
+def cached(expiry_time, cache_type=Cache):
+    def decorator(func):
+        sentinel = object()
+        cache = cache_type.get_instance()
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # noinspection PyProtectedMember
+            key = cache._hash_func((args, kwargs))
+            result = cache.get(key, default=sentinel, hashed_key=True)
+            if result is sentinel:
+                result = func(*args, **kwargs)
+                cache.set(key, result, expiry_time, hashed_key=True)
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+# noinspection PyTypeChecker
+def memory_cached(expiry_time):
+    return cached(expiry_time, MemoryCache)
---
+++ tests/test_cache.py
@@ -0,0 +1,116 @@
+import datetime
+import os
+import shutil
+import sys
+import time
+from unittest import TestCase
+
+try:
+    from unittest.mock import Mock
+except ImportError:
+    from mock import Mock
+
+DATA_FOLDER = "test_data"
+DATABASE_NAME = "test_database"
+
+
+def kodi_mocks():
+    # noinspection PyPep8Naming
+    class WindowMock(object):
+        def __init__(self):
+            self._cache = {}
+
+        def getProperty(self, key):
+            return self._cache.get(key, "")
+
+        def setProperty(self, key, value):
+            self._cache[key] = value
+
+    xbmc = Mock()
+    xbmc.translatePath.return_value = DATA_FOLDER
+    xbmcaddon = Mock()
+    xbmcaddon.Addon().getAddonInfo.return_value = DATABASE_NAME
+    xbmcgui = Mock()
+    xbmcgui.Window.return_value = WindowMock()
+    return {"xbmc": xbmc, "xbmcaddon": xbmcaddon, "xbmcgui": xbmcgui}
+
+
+sys.modules.update(kodi_mocks())
+from lib.cached import Cache, MemoryCache, _BaseCache  # noqa
+
+
+def with_values(*values):
+    def decorator(func):
+        def wrapper(self):
+            for value in values:
+                self.subTest("With {} value".format(value))
+                func(self, value)
+
+        return wrapper
+
+    return decorator
+
+
+class CacheTestCase(TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(DATA_FOLDER):
+            shutil.rmtree(DATA_FOLDER)
+
+    def test_singleton(self):
+        for clazz in (MemoryCache, Cache, _BaseCache):
+            self.assertIs(clazz.get_instance(), clazz.get_instance())
+            self.assertIsNot(clazz.get_instance(), clazz())
+        self.assertIsNot(Cache.get_instance(), _BaseCache.get_instance())
+        self.assertIsNot(Cache.get_instance(), MemoryCache.get_instance())
+        self.assertIsNot(_BaseCache.get_instance(), MemoryCache.get_instance())
+
+    @with_values(Cache, MemoryCache)
+    def test_expiry_time(self, clazz):
+        cache = clazz(os.path.join(DATA_FOLDER, "test_expiry_time.sqlite"))
+        key, value = "key", "value"
+        cache.set(key, value, datetime.timedelta(milliseconds=500))
+        start = time.time()
+        self.assertEqual(value, cache.get(key))
+        self.wait(0.4, start)
+        self.assertEqual(value, cache.get(key))
+        self.wait(0.5, start)
+        self.assertEqual(None, cache.get(key))
+
+    def test_clean_up(self):
+        key, value = "key", "value"
+        cache = Cache(os.path.join(DATA_FOLDER, "test_clean_up.sqlite"),
+                      cleanup_interval=datetime.timedelta(milliseconds=500))
+        start = time.time()
+        cache.set(key, value, datetime.timedelta(milliseconds=100))
+        self.assertFalse(cache.check_clean_up())
+        self.assertEqual(1, self.count(cache, key))
+        self.wait(0.4, start)
+        self.assertFalse(cache.check_clean_up())
+        self.assertEqual(1, self.count(cache, key))
+        self.wait(0.5, start)
+        self.assertTrue(cache.check_clean_up())
+        self.assertEqual(0, self.count(cache, key))
+
+    @with_values(Cache, MemoryCache)
+    def test_cache(self, clazz):
+        data = (1, "1", 1.1, True, None, {1}, frozenset([1]), [1], (1,), {1: 2, "1": "2"}, datetime.datetime.now())
+        expiry = datetime.timedelta(minutes=15)
+        cache = clazz(os.path.join(DATA_FOLDER, "test_cache.sqlite"))
+        for key in data:
+            for value in data:
+                cache.set(key, value, expiry)
+                self.assertEqual(value, cache.get(key))
+
+    @staticmethod
+    def count(cache, key):
+        cache._cursor.execute(
+            "SELECT COUNT(*) FROM `{}` WHERE key = ?".format(cache._table_name), (cache._hash_func(key),))
+        return cache._cursor.fetchone()[0]
+
+    @staticmethod
+    def wait(delay, start_time=None):
+        if start_time is None:
+            start_time = time.time()
+        while time.time() - start_time < delay:
+            pass
